b346e3fb6e7715b0b5810c7d03ed14c4878666f8
marcossilvano <markos.silvano@gmail.com>
2022-05-16 22:30:40 -0300

    Updating venv

--- map_gen/venv/bin/activate.fish
+++ map_gen/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/marcos/Projects/python/map_gen/venv"
+set -gx VIRTUAL_ENV "/home/marcos/GitHub/python-codes/map_gen/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
